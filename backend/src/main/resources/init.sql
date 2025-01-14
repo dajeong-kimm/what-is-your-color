4883b1c6f0ee1f6d27e4a33a045c3c3e27ef654f
DajeongKim <rlaekwjd6566@gmail.com>
2025-01-14 10:10:11 +0900

    fix: init.sql (ssafy -> ssafy_web_db)

--- backend/src/main/resources/init.sql
+++ backend/src/main/resources/init.sql
@@ -1,8 +1,8 @@
 -- 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS ssafy CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS ssafy_web_db CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 -- 데이터베이스 사용
-USE ssafy;
+USE ssafy_web_db;
 
 -- 사용자 테이블 생성
 CREATE TABLE user (
