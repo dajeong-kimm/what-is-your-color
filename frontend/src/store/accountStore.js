3a9018caeed022758405d0c5ae7918ca23fc0f1b
DajeongKim <rlaekwjd6566@gmail.com>
2025-01-16 16:31:03 +0900

    feat: 로그인/회원가입 프론트

--- frontend/src/store/accountStore.js
+++ frontend/src/store/accountStore.js
@@ -1,26 +1,41 @@
 import { requestLogin } from "../common/api/accountAPI";
 
 const state = {
-  token: null
+  token: localStorage.getItem('authToken') || null,  // localStorage에서 초기값 가져오기
+  login: !!localStorage.getItem('authToken'), // 로그인 상태 체크 (token이 있으면 로그인 상태로 간주)
 };
 
 const getters = {
-  getToken: state => {
-    return state.token;
-  }
+  getToken: (state) => state.token,
+  isLogin: (state) => state.login,
 };
 
 const mutations = {
   setToken: (state, token) => {
     state.token = token;
-  }
+    state.login = true;
+    localStorage.setItem('authToken', token);  // 로그인 시 로컬스토리지에 토큰 저장
+  },
+  removeToken: (state) => {
+    state.token = null;
+    state.login = false;
+    localStorage.removeItem('authToken');  // 로그아웃 시 로컬스토리지에서 토큰 삭제
+  },
 };
 
 const actions = {
   loginAction: async ({ commit }, loginData) => {
-    const response = await requestLogin(loginData);
-    commit("setToken", response.data.accessToken);
-  }
+    try {
+      const response = await requestLogin(loginData);
+      commit("setToken", response.data.accessToken);  // 로그인 후 토큰 저장
+    } catch (error) {
+      console.error("로그인 실패:", error);
+    }
+  },
+
+  logoutAction: ({ commit }) => {
+    commit("removeToken");  // 로그아웃 시 토큰 삭제
+  },
 };
 
 export default {
@@ -28,5 +43,5 @@ export default {
   state,
   getters,
   mutations,
-  actions
+  actions,
 };
--- frontend/src/views/main/main.vue
+++ frontend/src/views/main/main.vue
@@ -2,7 +2,9 @@
   <div class="main-wrapper">
     <main-header
       :height="'70px'"
-      @openLoginDialog="onOpenLoginDialog"/>
+      @openLoginDialog="onOpenLoginDialog"
+      @openRegisterDialog="onOpenRegisterDialog"/>
+      
     <div class="main-container">
       <aside class="sidebar hide-on-small" style="width: 240px;">
         <main-sidebar :width="'240px'"/>
@@ -15,6 +17,9 @@
     <login-dialog
       :open="loginDialogOpen"
       @closeLoginDialog="onCloseLoginDialog"/>
+    <register-dialog
+      :open="registerDialogOpen"
+      @closeRegisterDialog="onCloseRegisterDialog"/>  
   </div>
 </template>
 
@@ -59,6 +64,7 @@ import LoginDialog from './components/login-dialog'
 import MainHeader from './components/main-header'
 import MainSidebar from './components/main-sidebar'
 import MainFooter from './components/main-footer'
+import RegisterDialog from './components/register-dialog'
 
 export default {
   name: 'Main',
@@ -66,19 +72,31 @@ export default {
     MainHeader,
     MainSidebar,
     MainFooter,
-    LoginDialog
+    LoginDialog,
+    RegisterDialog,
   },
   data() {
     return {
-      loginDialogOpen: false
+      loginDialogOpen: false,
+      registerDialogOpen: false
     }
   },
   methods: {
+    // 로그인 다이얼로그열기기
     onOpenLoginDialog() {
       this.loginDialogOpen = true
     },
+    // 로그인 다이얼로그 닫기기
     onCloseLoginDialog() {
       this.loginDialogOpen = false
+    },
+    // 회원가입 다이얼로그 열기
+    onOpenRegisterDialog() {
+      this.registerDialogOpen = true
+    },
+    // 회원가입 다이얼로그 닫기
+    onCloseRegisterDialog() {
+      this.registerDialogOpen = false
     }
   }
 }
