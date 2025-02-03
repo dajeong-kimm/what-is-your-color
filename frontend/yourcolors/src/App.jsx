a792277c1cd3f5961fddf7267cac41a55229b060
seonguk <poem1999515@naver.com>
2025-02-03 11:58:15 +0900

    fix : 0203 mainpage3을 diagnosis의 Diagpage1로 이동, 작동확인

--- frontend/yourcolors/src/App.jsx
+++ frontend/yourcolors/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./features/start/Home";
 import MainPage from "./features/start/Mainpage";
 import Mainpage2 from "./features/start/Mainpage2";
-import Mainpage3 from "./features/start/Mainpage3";
+import Diagpage1 from "./features/diagnosis/Diagpage1";
 import TestPage from "./common/components/TestPage";
 import PersonalColorMain from './features/personalcolors/PersonalColorMain';
 // import PersonalDetail from './features/personalcolors/PersonalDetail';
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/mainpage" element={<MainPage />} />
         <Route path="/mainpage2" element={<Mainpage2 />} />
-        <Route path="/mainpage3" element={<Mainpage3 />} />
+        <Route path="/diagpage1" element={<Diagpage1 />} />
 
         <Route path="/test" element={<TestPage />} />
         <Route path="/personalcolors" element={<PersonalColorMain />} />
