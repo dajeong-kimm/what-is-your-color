da8852a2dbfa7af7fcabcbcaf54425bd9865ef8b
허준수 <gjwnstn515@naver.com>
2025-02-04 16:05:57 +0900

    fix: 백엔드 domain 폴더 구조 Merge

--- backend/yourcolors/src/main/java/com/ssafy/yourcolors/domain/personal/repository/PersonalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.yourcolors.domain.personal.repository;
-
-import com.ssafy.yourcolors.domain.personal.entity.PersonalColor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("personalPersonalRepository")
-public interface PersonalRepository extends JpaRepository<PersonalColor, Long> {
-}
--- backend/yourcolors/src/main/java/com/ssafy/yourcolors/domain/personal/service/PersonalService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.yourcolors.domain.personal.service;
-
-import com.ssafy.yourcolors.domain.personal.dto.PersonalColorResponseDto;
-import com.ssafy.yourcolors.domain.personal.entity.PersonalColor;
-
-public interface PersonalService {
-    PersonalColorResponseDto analyzePersonalColor(String imageUrl);
-    PersonalColor getPersonalById(Long personalId);
-}
