6d68904a7cb5323dd1079050751950916650048a
허준수 <gjwnstn515@naver.com>
2025-02-04 10:54:48 +0900

    chore: Repository 어느테이션 추가

--- backend/yourcolors/src/main/java/com/ssafy/yourcolors/domain/personal/repository/PersonalRepository.java
+++ backend/yourcolors/src/main/java/com/ssafy/yourcolors/domain/personal/repository/PersonalRepository.java
@@ -2,6 +2,8 @@ package com.ssafy.yourcolors.domain.personal.repository;
 
 import com.ssafy.yourcolors.domain.personal.entity.PersonalColor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("personalPersonalRepository")
 public interface PersonalRepository extends JpaRepository<PersonalColor, Long> {
 }
