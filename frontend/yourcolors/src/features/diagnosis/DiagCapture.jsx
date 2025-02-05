991d8101289ccce25c057341f423bf354879a4f6
unknown <hyunddoing@naver.com>
2025-02-05 14:24:48 +0900

    fix: DiagCapture update

--- frontend/yourcolors/src/features/diagnosis/DiagCapture.jsx
+++ frontend/yourcolors/src/features/diagnosis/DiagCapture.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Camera from './Diagcamera';  // Camera 컴포넌트 import
-import Background from "../../background/background/BackgGound";
+import Background from "../../background/background/BackGround";
 import Smallmain from "../../background/background/SmallMain";
 import Topbar from "../../button/top/TopBar";
 import Bottombar from "../../button/bottom/BottomBar";
