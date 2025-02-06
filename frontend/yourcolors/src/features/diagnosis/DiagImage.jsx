066d5a76c72d5d5beaffbd624fbfb069a5495baa
seonguk <poem1999515@naver.com>
2025-02-06 14:18:08 +0900

    fix : 0206 경로 수정

--- frontend/yourcolors/src/features/diagnosis/DiagImage.jsx
+++ frontend/yourcolors/src/features/diagnosis/DiagImage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import Background from '../../background/background/Background';
+import Background from '../../background/background/BackGround';
 import Smallmain from '../../background/background/SmallMain';
 import Topbar from '../../button/top/Topbar';
 import Bottombar from '../../button/bottom/Bottombar';
