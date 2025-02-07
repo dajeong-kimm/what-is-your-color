f799ad07471833371f1380365f62c559d4b4d4da
unknown <hyunddoing@naver.com>
2025-02-07 11:02:40 +0900

    fix: formatmessage, gpt comment 수정

--- backend/yourcolors/src/main/java/com/ssafy/yourcolors/domain/consult/service/TestImpl.java
+++ backend/yourcolors/src/main/java/com/ssafy/yourcolors/domain/consult/service/TestImpl.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Service
+//@Service
 public class TestImpl implements ConsultService {
 
     private final RestTemplate restTemplate = new RestTemplate();
