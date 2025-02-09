dc37d9e70ab32e73eb5474c8f420dc662ccb1e80
허준수 <gjwnstn515@naver.com>
2025-02-09 17:36:24 +0900

    chore: vite.config.js proxy 설정 dist에 적용하게

--- frontend/yourcolors/vite.config.js
+++ frontend/yourcolors/vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // 🔥 명시적으로 빌드 출력 폴더 설정
-  },
-
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:9000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist",  // 🔥 명시적으로 빌드 출력 폴더 설정
     },
-  },
+    server: mode === "development" ? {
+      proxy: {
+        "/api": {
+          target: "http://localhost:9000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
+        },
+      },
+    } : undefined,
+  };
 });
