6a5a5d828714c95a87ae31bc91fb47e845317bf9
허준수 <gjwnstn515@naver.com>
2025-02-09 19:32:45 +0900

    fix: DiagResult 충돌나는거 해결

--- frontend/yourcolors/src/features/diagnosis/DiagResult.jsx
+++ frontend/yourcolors/src/features/diagnosis/DiagResult.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import Background from "../../background/background/Background";
@@ -11,6 +11,7 @@ import BestWorst from "./BestWorst";
 import PersonalRecommend from "../recommend/PersonalRecommend";
 import LeftRightButton from "../../button/left-right-button/LeftRightButton"; // 🔹 추가!
 import "./DiagResult.css"; 
+import useStore from '../../store/UseStore'; //Zustand 상태관리 데이터
 
 const DiagResult = () => {
 
