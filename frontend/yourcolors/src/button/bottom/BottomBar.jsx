908ceaf37f3c16bd6613a47d38558e9ea85d1333
seonguk <poem1999515@naver.com>
2025-02-10 11:14:01 +0900

    feat : 0210 bottombutton과 모달 연결, 작동확인

--- frontend/yourcolors/src/button/bottom/BottomBar.jsx
+++ frontend/yourcolors/src/button/bottom/BottomBar.jsx
@@ -1,35 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import "./BottomBar.css";
 import Bottombutton from "./BottomButton";
 import CloseButton from "../closebutton/CloseButton"; // 종료하기 버튼
 
-const Bottombar = () => {
-  const location = useLocation();
-  const [activeButton, setActiveButton] = useState("");
-
-  // 버튼 정보 배열 (첫 번째 버튼의 to 값을 "/diagresult"로 변경)
+const Bottombar = ({ currentStep, setCurrentStep }) => {
+  // 각 버튼은 모달의 인덱스와 매핑됩니다.
   const buttons = [
-    { to: "/diagresult", label: "진단결과", name: "diagnosis" },
-    { to: "/personalcolors/1", label: "컬러상세", name: "colorDetails" },
-    { to: "/bestworst", label: "BEST/WORST", name: "bestWorst" },
-    { to: "/recommend", label: "화장품추천", name: "cosmeticRecommend" },
+    { index: 0, label: "진단결과", name: "diagnosis" },
+    { index: 1, label: "컬러상세", name: "colorDetails" },
+    { index: 2, label: "BEST/WORST", name: "bestWorst" },
+    { index: 3, label: "화장품추천", name: "cosmeticRecommend" },
   ];
 
-  // 현재 경로에 따라 activeButton 설정
-  useEffect(() => {
-    const currentPath = location.pathname;
-    const active = buttons.find((button) => button.to === currentPath);
-    if (active) {
-      setActiveButton(active.name);
-    } else {
-      // 만약 현재 경로가 버튼에 해당하지 않는다면 기본값(예: 첫 버튼) 설정 가능
-      setActiveButton(buttons[0].name);
-    }
-  }, [location.pathname, buttons]);
-
-  const handleButtonClick = (buttonName) => {
-    setActiveButton(buttonName);
+  const handleButtonClick = (index) => {
+    setCurrentStep(index);
   };
 
   return (
@@ -39,10 +23,9 @@ const Bottombar = () => {
           {buttons.map((button) => (
             <Bottombutton
               key={button.name}
-              to={button.to}
               label={button.label}
-              isActive={activeButton === button.name}
-              onClick={() => handleButtonClick(button.name)}
+              isActive={currentStep === button.index}
+              onClick={() => handleButtonClick(button.index)}
               className={button.name}
             />
           ))}
