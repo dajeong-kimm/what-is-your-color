8c9e7bc91462d5dae89be895e0cd8efa1001254f
seonguk <poem1999515@naver.com>
2025-02-12 10:54:14 +0900

    fix : result 페이지 버튼 라우팅, personalcolor detail페이지, startbutton 구조, css수정

--- frontend/yourcolors/src/features/start/Home.jsx
+++ frontend/yourcolors/src/features/start/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Background from "../../background/background/Background";
 import StartImage from "../../assets/Start.png"; // 이미지 파일 불러오기
-import Topbutton from "../../button/top/TopButton"; // 버튼 재사용
+import StartButton from "./StartButton";
 import "./Home.css";
 
 const Home = () => {
@@ -13,7 +13,7 @@ const Home = () => {
         
         {/* 시작하기 버튼 (이미지 아래 배치) */}
         <div className="home-button">
-          <Topbutton to="/Mainpage" label="시작하기" />
+          <StartButton to="/Mainpage" label="시작하기" />
         </div>
       </div>
     </Background>
