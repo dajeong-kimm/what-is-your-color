18d2dc57192250ac655fe1e117db8591358a46d4
miseo <rong030304@naver.com>
2025-02-13 17:59:47 +0900

    feat:personaldefine update

--- frontend/yourcolors/src/button/top/TopBar.jsx
+++ frontend/yourcolors/src/button/top/TopBar.jsx
@@ -5,6 +5,7 @@ import './TopBar.css';
 import Topbutton from './TopButton';
 import Logo from "../../assets/Logo.png"; // 이미지 import
 
+
 const Topbar = () => {
   const location = useLocation();
 
@@ -26,7 +27,7 @@ const Topbar = () => {
 
   // 버튼 정보 배열
   const buttons = [
-    { to: '/choice', label: '퍼스널컬러란?', name: 'mean' },
+    { to: '/personaldefine', label: '퍼스널컬러란?', name: 'define' },
     { to: '/personalcolors', label: '퍼스널컬러 종류', name: 'personalcolor' },
     { to: '/mainpage', label: '메인페이지', name: 'main' },
     { to: '/', label: '처음으로', name: 'start' },
